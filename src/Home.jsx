6996c5ba4e60738176252f25867f8711c91313b0
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-05-28 10:44:24 +0545

    updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Background from "./assets/background.jpg";
-import Logo from "./assets/logo.png";
+import Logo from "./assets/Logo.png";
 
 const Home = () => {
   
